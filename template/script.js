63f6ecfd052c2869d728587201e0e498eb261eb3
kyoowon <kyoowon95@gmail.com>
2021-01-27 16:17:35 +0900

    M

---
+++ template/script.js
@@ -0,0 +1,35 @@
+
+
+window.console = window.console || function(t) {};
+
+if (document.location.search.match(/type=embed/gi)) {
+    window.parent.postMessage("resize", "*");
+
+
+<script src="https://cpwebassets.codepen.io/assets/common/stopExecutionOnTimeout-157cd5b220a5c80d4ff8e0e70ac069bffd87a61252088146915e8726e5d9f147.js"></script>
+
+window.addEventListener('mousemove', handleMouseMove);
+window.addEventListener('resize', handleWindowResize);
+
+const spansSlow = document.querySelectorAll('.spanSlow');
+const spansFast = document.querySelectorAll('.spanFast');
+
+let width = window.innerWidth;
+
+function handleMouseMove(e) {
+  let normalizedPosition = e.pageX / (width / 2) - 1;
+  let speedSlow = 100 * normalizedPosition;
+  let speedFast = 200 * normalizedPosition;
+  spansSlow.forEach(span => {
+    span.style.transform = `translate(${speedSlow}px)`;
+  });
+  spansFast.forEach(span => {
+    span.style.transform = `translate(${speedFast}px)`;
+  });
+}
+//we need to recalculate width when the window is resized
+function handleWindowResize() {
+  width = window.innerWidth;
+}
+
+<script src="https://cpwebassets.codepen.io/assets/editor/iframe/iframeRefreshCSS-bb9a2ba1f03f6147732cb3cd52ac86c6b24524aa87a05ed0b726f11e46d7e277.js"></script>
