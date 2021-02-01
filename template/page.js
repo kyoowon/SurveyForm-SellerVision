2f4d3a6fc61117fb94070207508296e8c2c5895e
kyoowon <kyoowon95@gmail.com>
2021-02-01 17:14:58 +0900

    add select result

--- template/page.js
+++ template/page.js
@@ -1,13 +1,20 @@
-function click_select(id)
-{
-  var color = document.getElementById(id);
-  if (color.value === 'yes')
-  {
-	document.getElementById(id).style.backgroundColor= '#fafafa';
-	color.value ='no';
-  }  
-  else {
-	document.getElementById(id).style.backgroundColor= '#c1c1c1';
-	color.value = 'yes';
+var total_list = []
+var select_list = {}
+
+function click_select(q, id) {
+    var color = document.getElementById(id);
+    if (color.value === 'yes')
+    {
+      document.getElementById(id).style.backgroundColor= '#fafafa';
+      color.value ='no';
+    }  
+    else {
+      document.getElementById(id).style.backgroundColor= '#c1c1c1';
+      color.value = 'yes';
+      select_list = {q, id};
   }
+}
+
+function next_page() {
+  total_list.push(select_list);
 }
