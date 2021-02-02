98a25443445f4a2ad9ed10fe826c62133653106c
qor4 <qor_4@naver.com>
2021-02-02 10:42:35 +0900

    s

--- template/page.js
+++ template/page.js
@@ -1,13 +1,26 @@
-function click_select(id)
+function click_select(id, state, val)
 {
+  var pre_id = id;
+  var answer = 0;
   var color = document.getElementById(id);
-  if (color.value === 'yes')
-  {
-	document.getElementById(id).style.backgroundColor= '#fafafa';
-	color.value ='no';
-  }  
-  else {
-	document.getElementById(id).style.backgroundColor= '#c1c1c1';
-	color.value = 'yes';
+  if (state==1){
+    if (color.value === 'yes')
+    {
+    document.getElementById(id).style.backgroundColor= '#fafafa';
+    if(answer == val){
+      answer=0
+    }
+    color.value ='no';
+    }  
+    else {
+      if (answer != 0){
+        document.getElementById(pre_id).style.backgroundColor= '#fafafa';
+        document.getElementById(pre_id).color.value ='no';
+      }
+    document.getElementById(id).style.backgroundColor= '#c1c1c1';
+    color.value = 'yes';
+    answer = value;
+    }
   }
+  
 }
