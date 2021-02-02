1b50c74d97752f2c0c22d62ae1ca04c2f74c0de1
kyoowon <kyoowon95@gmail.com>
2021-02-02 10:11:45 +0900

    add select function

--- template/page.js
+++ template/page.js
@@ -1,20 +1,49 @@
 var total_list = []
 var select_list = {}
 
+function find_id(q)
+{
+  for(lst in total_list)
+  {
+    if(lst[0] == q)
+      return (lst);
+  }
+  return null;
+}
+
 function click_select(q, id) {
     var color = document.getElementById(id);
-    if (color.value === 'yes')
+    if (color.value == 'yes')
     {
-      document.getElementById(id).style.backgroundColor= '#fafafa';
+      if (q % 2 != 0)
+        color.style.backgroundColor = '#fafafa';
+      else
+        color.style.backgroundColor = '#181f39';
       color.value ='no';
     }  
     else {
-      document.getElementById(id).style.backgroundColor= '#c1c1c1';
+      if (select_list.id != null)
+      {
+        if (q % 2 != 0)
+          document.getElementById(select_list.id).style.backgroundColor = '#fafafa';
+        else
+          document.getElementById(select_list.id).style.backgroundColor = '#181f39';
+      }
+      color.style.backgroundColor= '#c1c1c1';
       color.value = 'yes';
       select_list = {q, id};
   }
 }
 
 function next_page() {
-  total_list.push(select_list);
+  for (lst in total_list)
+  {
+    if (lst[0] == select_list.q){
+      lst = select_list;
+      return ;
+    }
+  }
+  if (select_list.id != null)
+    total_list.push(select_list);
+  select_list = {};
 }
