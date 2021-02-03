54a74e905aa0c16b901aa916bb8307566d1c558c
kyoowon <kyoowon95@gmail.com>
2021-02-03 16:17:59 +0900

    a

--- template/page.js
+++ template/page.js
@@ -2,7 +2,6 @@
 var total_list = []
 var select_list = {}
 
-
 function find_id(q)
 {
   for(i in total_list)
@@ -67,7 +66,7 @@ function click_select(q, id) {
         else
           document.getElementById(select_list.id).style.color = '#fafafa';
     }
-    else if (total_list[idx] != null)
+    if (total_list[idx] != null)
     {
         document.getElementById(total_list[idx].id).style.textDecoration = 'none';
         if (total_list[idx].q % 2 == 1)
@@ -86,6 +85,7 @@ function next_page() {
   {
     if (total_list[i].q == select_list.q){
       total_list[i] = select_list;
+      select_list = {};
       return ;
     }
   }
@@ -95,7 +95,6 @@ function next_page() {
 }
 
 function submit_form(){
-
   for(i in total_list)
   {
     frist = total_list[i].id.indexOf("a");
