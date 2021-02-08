54d5bf3db4a3c2b93c70c0fe0c4dc6b1b262b1a8
qor4 <qor_4@naver.com>
2021-02-08 16:08:50 +0900

    db

--- server/sql.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    surveyList : {
-        query: 'select * from survey_re'
-    }
-}
--- test/testnode.js
+++
@@ -1,5 +0,0 @@
-var te = require('./test.js');
-import total_list from '/test.js'
-// import * as test from './test.js';
-console.log(total_list);
-// console.log(te.b);
